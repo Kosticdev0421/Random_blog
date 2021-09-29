c8f761120c21ae42e8fcf3e64489b4b442ecf349
Markodev0421 <mpribilovic2@gmail.com>
2021-09-29 17:37:57 -0400

    design satisfactory

--- random-thoughts-blog-client-app/src/App.js
+++ random-thoughts-blog-client-app/src/App.js
@@ -1,15 +1,49 @@
+// Components
+
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Compose from "./pages/compose/Compose";
 import Settings from "./pages/settings/Settings";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+
+// Router
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 export default function App() {
+  // pseudo user to play with the design portion, no logged in user
+  const user = false;
   return (
-    <div>
+    <Router>
       <TopBar />
-      <Settings />
-    </div>
+      <Switch>
+        <Route exact  path="/">
+          <Home />
+        </Route> 
+        <Route path="/register">
+        {/* if there is a user go to home IF NOT then go to register */}
+          {user ? <Home /> : <Register/>}
+        </Route> 
+        <Route path="/login">
+        {/* if user is logged in go home IF NOT then go log in */}
+          {user ? <Home /> : <Login />}
+        </Route>
+        <Route path="/compose">
+        {/* if user logged in they can composer IF NOT they need to register */}
+          {user ? <Compose /> : <Register />}
+        </Route>
+        <Route path="/settings">
+        {/*user logged in goes to settings IF NOT then they have to register */}
+          {user ? <Settings /> : <Register />}
+        </Route>
+        <Route path="/post/:postId">
+        {/* no conditions needed because everyone can view this single page */}
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
--- random-thoughts-blog-client-app/src/components/topbar/TopBar.jsx
+++ random-thoughts-blog-client-app/src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import './topbar.css';
 
 function TopBar() {
+  const user = false;
   return (
       <div className="top">
        <div className="topLeft">
@@ -8,18 +10,47 @@ function TopBar() {
        </div>
        <div className="topCenter">
           <ul className="topList">
-            <li className="topListItem">HOME</li>
-            <li className="topListItem">ABOUT</li>
-            <li className="topListItem">CONTACT</li>
-            <li className="topListItem">COMPOSE</li>
-            <li className="topListItem">LOGOUT</li>
+            <li className="topListItem">
+              <Link className="link" to="/">HOME</Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/about">ABOUT</Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/contact">CONTACT</Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/compose">COMPOSE</Link>
+            </li>
+            <li className="topListItem">
+              {user && "LOGOUT"}
+            </li>
           </ul>  
        </div>
        <div className="topRight">
-          <img 
+       {/*this controls what is seen.  If user, show profile pic OR show the login register option */}
+        {
+          user ? (
+            <img 
             className="topImg"
             src="https://static.thenounproject.com/png/94090-200.png" alt="girl-dev" 
           />
+          ) : (
+            <ul className="topList">
+              <li className="topListItem">
+                <Link className="link" to="/login">
+                  LOGIN
+                </Link>
+              </li>
+              <li className="topListItem">
+                <Link className="link" to="/register">
+                  REGISTER  
+                </Link>
+              </li>
+            </ul>
+          )
+        }
+          
           <i className="topSearchIcon fas fa-search"></i>
 
        </div>
