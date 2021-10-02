7a75c501c3a5835fb58838adb12b53ed6b9a1e1d
Markodev0421 <mpribilovic2@gmail.com>
2021-10-01 20:14:03 -0400

    style changes

--- blog-clientside/src/components/header/Header.jsx
+++ blog-clientside/src/components/header/Header.jsx
@@ -5,10 +5,10 @@ export default function Header() {
   return (
     <div className="header">
         <div className="headerTitles">
-            <span className="headerTitleLg">Random Thoughts</span>
-            <span className="headerTitleSm">Blog</span>
+            <span className="headerTitleSm">Random Thoughts</span>
+            <span className="headerTitleLg">Blog</span>
         </div>
-        <img className="headerImg" src="https://image.freepik.com/free-vector/modern-abstract-background_1048-1003.jpg" alt="abstract-art" />
+        <img className="headerImg" src="https://images.unsplash.com/photo-1602498456745-e9503b30470b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80" alt="abstract-art" />
     </div>
   )
 }
